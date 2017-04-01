7dac05bf86cc3cc1294bb4611ee0a96e8c9b5bed
yuanzhengqiang <273816488@qq.com>
2017-04-01 09:16:37 +0800

    小Demo
    通过控制stepper来控制进度条和文本框的值

--- UIStepper/ViewController.swift
+++ UIStepper/ViewController.swift
@@ -13,11 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        progress1.progress = 0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var progress1: UIProgressView!
+    @IBOutlet weak var stepper: UIStepper!
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        textField.text = String(format: "%g", sender.value)
+        progress1.progress = Float((sender.value)/100)
     }
 
 
